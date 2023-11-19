5a58ea95858dc35f8058a4759b2baeb322fd2ad6
NanJunLYS <18906571516@163.com>
2023-11-19 11:01:55 +0800

    v0.0.6.3 补充类型判断

--- gamenavigator/keyboard_mouse_simulation.py
+++ gamenavigator/keyboard_mouse_simulation.py
@@ -13,12 +13,22 @@ from . import log
 
 def mouse_move_to(pos: Pos, duration: float = 0.0) -> None:
     """ 模拟鼠标移动 """
+    if not isinstance(pos, Pos):
+        raise TypeError("pos must be Pos")
+    if not isinstance(duration, float):
+        raise TypeError("duration must be float")
     pyautogui.moveTo(pos.x, pos.y, duration)
     log.debug(f"mouse move to {pos}")
 
 
 def mouse_click_position(pos: Pos, button: str = "left") -> None:
     """ 模拟鼠标点击坐标pos """
+    if not isinstance(pos, Pos):
+        raise TypeError("pos must be Pos")
+    if not isinstance(button, str):
+        raise TypeError("button must be str")
+    if button not in ("left", "right"):
+        raise ValueError("button must be left or right")
     if button == "left":
         down = win32con.MOUSEEVENTF_LEFTDOWN
         up = win32con.MOUSEEVENTF_LEFTUP
@@ -41,6 +51,12 @@ def mouse_scroll(scale: int, count: int = 1) -> None:
         scale (int): 刻度
         count (int): 次数
     """
+    if not isinstance(scale, int):
+        raise TypeError("scale must be int")
+    if not isinstance(count, int):
+        raise TypeError("count must be int")
+    if count < 1:
+        raise ValueError("count must be greater than 0")
     for _ in range(count):
         try:
             pyautogui.scroll(scale)
@@ -59,6 +75,12 @@ def mouse_drag(start: Pos, end: Pos, button='left') -> None:
         end (Pos): 终点
         button (str): 鼠标按键. (default left)
     """
+    if not isinstance(start, Pos):
+        raise TypeError("start must be Pos")
+    if not isinstance(end, Pos):
+        raise TypeError("end must be Pos")
+    if not button in ('left', 'right', 'middle'):
+        raise ValueError("button must be left, right or middle")
     x1, y1 = start.x, start.y
     x2, y2 = end.x, end.y
     pyautogui.mouseDown(x1, y1, button)
@@ -73,6 +95,8 @@ def mouse_click(button: str) -> None:
     Args:
         button (str): left or right or middle (default left)
     """
+    if not isinstance(button, str):
+        raise TypeError("button must be str")
     button = button.lower()
     if button == "left":
         pyautogui.leftClick()
@@ -93,18 +117,24 @@ def mouse_double_click() -> None:
 
 def keyboard_down(key: str) -> None:
     """ 模拟键盘按键按下 """
+    if not isinstance(key, str):
+        raise TypeError("key must be str")
     pyautogui.keyDown(key)
     log.debug(f"keyboard down: {key}")
 
 
 def keyboard_up(key: str) -> None:
     """ 模拟键盘按键松开 """
+    if not isinstance(key, str):
+        raise TypeError("key must be str")
     pyautogui.keyUp(key)
     log.debug(f"keyboard up: {key}")
 
 
 def keyboard_press(key: str) -> None:
     """ 模拟键盘按键轻按 """
+    if not isinstance(key, str):
+        raise TypeError("key must be str")
     pyautogui.press(key)
     log.debug(f"keyboard press: {key}")
 
