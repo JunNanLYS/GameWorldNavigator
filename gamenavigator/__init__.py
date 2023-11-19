5d7a16235f643d3a41f98a72ecd162648e6129e4
NanJunLYS <18906571516@163.com>
2023-11-19 11:00:32 +0800

    Update __init__.py

--- gamenavigator/__init__.py
+++ gamenavigator/__init__.py
@@ -1,5 +1,5 @@
 from . import config
-from .keyboard_mouse_simulation import (mouse_scroll, mouse_move_to, mouse_press_and_move, mouse_click_position,
+from .keyboard_mouse_simulation import (mouse_scroll, mouse_move_to, mouse_click_position, mouse_drag,
                                         keyboard_up, keyboard_down, keyboard_press)
 from .image_recognition import match_template, where_img
 from .game_controller import Game, GameController
